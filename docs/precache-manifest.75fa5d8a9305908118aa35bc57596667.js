24d6ad49c1ccec3c2cba5c8e9e00f34982137177
derainRS <derian.rods@gmail.com>
2020-10-13 13:15:08 +0200

    docs

---
+++ docs/precache-manifest.75fa5d8a9305908118aa35bc57596667.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848ffcb18d625635b26c6f83079ccb05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da510ead4b3a214b624a",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "9ffc0cd5558716398aca",
+    "url": "/static/css/main.eced9001.chunk.css"
+  },
+  {
+    "revision": "da510ead4b3a214b624a",
+    "url": "/static/js/2.8160718d.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.8160718d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffc0cd5558716398aca",
+    "url": "/static/js/main.2b88985b.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "0d3e61eaa1a74833c7fc23e3e39a8198",
+    "url": "/static/media/040-earth.0d3e61ea.svg"
+  }
+]);
